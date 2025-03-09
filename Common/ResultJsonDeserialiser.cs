6c4d48300c57c1ad48fd861863e338294bacc64c
ThereFox <alexandrgrizunow@gmail.com>
2025-03-09 13:29:39 +0400

    add result deserialiser

---
+++ Common/ResultJsonDeserialiser.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using CSharpFunctionalExtensions;
+
+namespace Common;
+
+public static class ResultJsonDeserialiser
+{
+    public static Result<T> Deserialise<T>(string json)
+    {
+        try
+        {
+            var result = JsonSerializer.Deserialize<T>(json);
+
+            if (result == null)
+            {
+                return Result.Failure<T>("Could not deserialise");
+            }
+            return result;
+        }
+        catch (Exception e)
+        {
+            return Result.Failure<T>(e.Message);
+        }
+    }
+    public static Result<object> Deserialise(Type type, string json)
+    {
+        try
+        {
+            var result = JsonSerializer.Deserialize(
+                json,
+                type,
+                new JsonSerializerOptions()
+                {
+                    IncludeFields = true,
+                    DefaultIgnoreCondition = JsonIgnoreCondition.Never
+                }
+                );
+
+            if (result == null)
+            {
+                return Result.Failure<object>("Could not deserialise");
+            }
+            return result;
+        }
+        catch (Exception e)
+        {
+            return Result.Failure<object>(e.Message);
+        }
+    }
+}
